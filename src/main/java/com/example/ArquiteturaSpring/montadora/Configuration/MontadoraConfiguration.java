8097e9f5d8bdebec60c6e75a45b199be6654ee1d
leandro1027 <126489808+leandro1027@users.noreply.github.com>
2025-02-27 11:49:07 -0300

    .

--- src/main/java/com/example/ArquiteturaSpring/montadora/Configuration/MontadoraConfiguration.java
+++ src/main/java/com/example/ArquiteturaSpring/montadora/Configuration/MontadoraConfiguration.java
@@ -1,4 +1,7 @@
 package com.example.ArquiteturaSpring.montadora.Configuration;
 
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class MontadoraConfiguration {
 }
